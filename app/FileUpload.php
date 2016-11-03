d3a7489a0398866657f7b6bc4c60434c1f4413dd
Andreas Elia <andreaselia@live.co.uk>
2016-11-03 19:52:04 +0000

    Progress

---
+++ app/FileUpload.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class FileUpload extends Model
+{
+    public function upload()
+    {
+        //
+    }
+
+    public function get()
+    {
+        //
+    }
+
+    public function delete()
+    {
+        //
+    }
+}
---
+++ app/Scrape.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Scrape extends Model
+{
+    public function funko()
+    {
+        //
+    }
+}
