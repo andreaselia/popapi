c3a09b9b23c70131dafd0a02fe25ad0c73ea1d09
Andreas Elia <andreaselia@live.co.uk>
2016-11-03 20:59:23 +0000

    Progress

--- app/FileUpload.php
+++ app/FileUpload.php
@@ -2,15 +2,20 @@
 
 namespace App;
 
+use Aws\S3\Exception\S3Exception;
 use Illuminate\Database\Eloquent\Model;
 
 class FileUpload extends Model
 {
-    protected $aws;
+    protected $s3;
 
     public function initalizeAws()
     {
-        //
+        try {
+            $this->s3 = AWS::createClient('s3');
+        } catch (S3Exception $e) {
+            echo $e->getMessage();
+        }
     }
 
     public function link($id)
@@ -18,9 +23,19 @@ class FileUpload extends Model
         //
     }
 
-    public function upload($input, $output)
+    public function upload($bucket, $key, $file)
     {
-        //
+        $this->initalizeAws();
+
+        try {
+            $this->s3->putObject([
+                'Bucket'     => $bucket,
+                'Key'        => $key,
+                'SourceFile' => $file
+            ]);
+        } catch (S3Exception $e) {
+            echo $e->getMessage();
+        }
     }
 
     public function delete($id)
